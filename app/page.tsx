0f05e93f7449b458345a916bd3263603e0435497
Nataliia Svystula <105055646+Nataliia0202@users.noreply.github.com>
2024-08-09 20:05:27 +0300

    created header

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,6 @@
-import { Button } from "@/components/ui/button";
+
 
 
 export default function Home() {
-  return (
-    <h1>Home
-      <Button variant="outline">Корзина</Button>
-    </h1>
-  );
+  return <></>
 }
