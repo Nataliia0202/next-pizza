1c326454d8add6de1f83bad4e601f913871d7a09
Nataliia Svystula <105055646+Nataliia0202@users.noreply.github.com>
2024-08-14 21:23:33 +0300

    added database

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
       </Container>
       <TopBar />
       <Container className=" mt-10 pb-14">
-        <div className="flex gap-[60px]">
+        <div className="flex gap-[80px]">
           {/* { Фильтрация} */}
 
           <div className="w-[250px]">
@@ -30,14 +30,28 @@ export default function Home() {
                   items:[{price: 50}]
                 },
                 {
-                  id:1,
+                  id:2,
                   name: 'Cheese sauce',
                   imageUrl: "https://media.istockphoto.com/id/1413684626/ru/%D1%84%D0%BE%D1%82%D0%BE/%D0%BA%D0%BB%D0%B0%D1%81%D1%81%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B0%D1%8F-%D0%BF%D0%B8%D1%86%D1%86%D0%B0-%D0%BF%D0%B5%D0%BF%D0%BF%D0%B5%D1%80%D0%BE%D0%BD%D0%B8-%D1%81-%D0%BD%D0%B0%D1%80%D0%B5%D0%B7%D0%B0%D0%BD%D0%BD%D1%8B%D0%BC%D0%B8-%D0%BB%D0%BE%D0%BC%D1%82%D0%B8%D0%BA%D0%B0%D0%BC%D0%B8-%D0%B2%D1%8B%D0%B4%D0%B5%D0%BB%D0%B5%D0%BD%D0%BD%D1%8B%D0%BC%D0%B8-%D0%BD%D0%B0-%D0%B1%D0%B5%D0%BB%D0%BE%D0%BC.jpg?s=612x612&w=0&k=20&c=8s9XzBQNFdHBZPxzukGFXNk5jICoBrEb5wA0TZ3vfgk=", 
                   price: 50,
                   items:[{price: 50}]
                 },
                 {
-                  id:1,
+                  id:3,
+                  name: 'Cheese sauce',
+                  imageUrl: "https://media.istockphoto.com/id/1413684626/ru/%D1%84%D0%BE%D1%82%D0%BE/%D0%BA%D0%BB%D0%B0%D1%81%D1%81%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B0%D1%8F-%D0%BF%D0%B8%D1%86%D1%86%D0%B0-%D0%BF%D0%B5%D0%BF%D0%BF%D0%B5%D1%80%D0%BE%D0%BD%D0%B8-%D1%81-%D0%BD%D0%B0%D1%80%D0%B5%D0%B7%D0%B0%D0%BD%D0%BD%D1%8B%D0%BC%D0%B8-%D0%BB%D0%BE%D0%BC%D1%82%D0%B8%D0%BA%D0%B0%D0%BC%D0%B8-%D0%B2%D1%8B%D0%B4%D0%B5%D0%BB%D0%B5%D0%BD%D0%BD%D1%8B%D0%BC%D0%B8-%D0%BD%D0%B0-%D0%B1%D0%B5%D0%BB%D0%BE%D0%BC.jpg?s=612x612&w=0&k=20&c=8s9XzBQNFdHBZPxzukGFXNk5jICoBrEb5wA0TZ3vfgk=", 
+                  price: 50,
+                  items:[{price: 50}]
+                },
+                {
+                  id:4,
+                  name: 'Cheese sauce',
+                  imageUrl: "https://media.istockphoto.com/id/1413684626/ru/%D1%84%D0%BE%D1%82%D0%BE/%D0%BA%D0%BB%D0%B0%D1%81%D1%81%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B0%D1%8F-%D0%BF%D0%B8%D1%86%D1%86%D0%B0-%D0%BF%D0%B5%D0%BF%D0%BF%D0%B5%D1%80%D0%BE%D0%BD%D0%B8-%D1%81-%D0%BD%D0%B0%D1%80%D0%B5%D0%B7%D0%B0%D0%BD%D0%BD%D1%8B%D0%BC%D0%B8-%D0%BB%D0%BE%D0%BC%D1%82%D0%B8%D0%BA%D0%B0%D0%BC%D0%B8-%D0%B2%D1%8B%D0%B4%D0%B5%D0%BB%D0%B5%D0%BD%D0%BD%D1%8B%D0%BC%D0%B8-%D0%BD%D0%B0-%D0%B1%D0%B5%D0%BB%D0%BE%D0%BC.jpg?s=612x612&w=0&k=20&c=8s9XzBQNFdHBZPxzukGFXNk5jICoBrEb5wA0TZ3vfgk=", 
+                  price: 50,
+                  items:[{price: 50}]
+                },
+                {
+                  id:5,
                   name: 'Cheese sauce',
                   imageUrl: "https://media.istockphoto.com/id/1413684626/ru/%D1%84%D0%BE%D1%82%D0%BE/%D0%BA%D0%BB%D0%B0%D1%81%D1%81%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B0%D1%8F-%D0%BF%D0%B8%D1%86%D1%86%D0%B0-%D0%BF%D0%B5%D0%BF%D0%BF%D0%B5%D1%80%D0%BE%D0%BD%D0%B8-%D1%81-%D0%BD%D0%B0%D1%80%D0%B5%D0%B7%D0%B0%D0%BD%D0%BD%D1%8B%D0%BC%D0%B8-%D0%BB%D0%BE%D0%BC%D1%82%D0%B8%D0%BA%D0%B0%D0%BC%D0%B8-%D0%B2%D1%8B%D0%B4%D0%B5%D0%BB%D0%B5%D0%BD%D0%BD%D1%8B%D0%BC%D0%B8-%D0%BD%D0%B0-%D0%B1%D0%B5%D0%BB%D0%BE%D0%BC.jpg?s=612x612&w=0&k=20&c=8s9XzBQNFdHBZPxzukGFXNk5jICoBrEb5wA0TZ3vfgk=", 
                   price: 50,
@@ -45,6 +59,47 @@ export default function Home() {
                 }
               ]} 
               categoryId={1}/>
+
+<ProductsGroupList 
+              title={"Combo"} 
+              items={[
+                {
+                  id:1,
+                  name: 'Cheese sauce',
+                  imageUrl: "https://media.istockphoto.com/id/1413684626/ru/%D1%84%D0%BE%D1%82%D0%BE/%D0%BA%D0%BB%D0%B0%D1%81%D1%81%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B0%D1%8F-%D0%BF%D0%B8%D1%86%D1%86%D0%B0-%D0%BF%D0%B5%D0%BF%D0%BF%D0%B5%D1%80%D0%BE%D0%BD%D0%B8-%D1%81-%D0%BD%D0%B0%D1%80%D0%B5%D0%B7%D0%B0%D0%BD%D0%BD%D1%8B%D0%BC%D0%B8-%D0%BB%D0%BE%D0%BC%D1%82%D0%B8%D0%BA%D0%B0%D0%BC%D0%B8-%D0%B2%D1%8B%D0%B4%D0%B5%D0%BB%D0%B5%D0%BD%D0%BD%D1%8B%D0%BC%D0%B8-%D0%BD%D0%B0-%D0%B1%D0%B5%D0%BB%D0%BE%D0%BC.jpg?s=612x612&w=0&k=20&c=8s9XzBQNFdHBZPxzukGFXNk5jICoBrEb5wA0TZ3vfgk=", 
+                  price: 50,
+                  items:[{price: 50}]
+                },
+                {
+                  id:2,
+                  name: 'Cheese sauce',
+                  imageUrl: "https://media.istockphoto.com/id/1413684626/ru/%D1%84%D0%BE%D1%82%D0%BE/%D0%BA%D0%BB%D0%B0%D1%81%D1%81%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B0%D1%8F-%D0%BF%D0%B8%D1%86%D1%86%D0%B0-%D0%BF%D0%B5%D0%BF%D0%BF%D0%B5%D1%80%D0%BE%D0%BD%D0%B8-%D1%81-%D0%BD%D0%B0%D1%80%D0%B5%D0%B7%D0%B0%D0%BD%D0%BD%D1%8B%D0%BC%D0%B8-%D0%BB%D0%BE%D0%BC%D1%82%D0%B8%D0%BA%D0%B0%D0%BC%D0%B8-%D0%B2%D1%8B%D0%B4%D0%B5%D0%BB%D0%B5%D0%BD%D0%BD%D1%8B%D0%BC%D0%B8-%D0%BD%D0%B0-%D0%B1%D0%B5%D0%BB%D0%BE%D0%BC.jpg?s=612x612&w=0&k=20&c=8s9XzBQNFdHBZPxzukGFXNk5jICoBrEb5wA0TZ3vfgk=", 
+                  price: 50,
+                  items:[{price: 50}]
+                },
+                {
+                  id:3,
+                  name: 'Cheese sauce',
+                  imageUrl: "https://media.istockphoto.com/id/1413684626/ru/%D1%84%D0%BE%D1%82%D0%BE/%D0%BA%D0%BB%D0%B0%D1%81%D1%81%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B0%D1%8F-%D0%BF%D0%B8%D1%86%D1%86%D0%B0-%D0%BF%D0%B5%D0%BF%D0%BF%D0%B5%D1%80%D0%BE%D0%BD%D0%B8-%D1%81-%D0%BD%D0%B0%D1%80%D0%B5%D0%B7%D0%B0%D0%BD%D0%BD%D1%8B%D0%BC%D0%B8-%D0%BB%D0%BE%D0%BC%D1%82%D0%B8%D0%BA%D0%B0%D0%BC%D0%B8-%D0%B2%D1%8B%D0%B4%D0%B5%D0%BB%D0%B5%D0%BD%D0%BD%D1%8B%D0%BC%D0%B8-%D0%BD%D0%B0-%D0%B1%D0%B5%D0%BB%D0%BE%D0%BC.jpg?s=612x612&w=0&k=20&c=8s9XzBQNFdHBZPxzukGFXNk5jICoBrEb5wA0TZ3vfgk=", 
+                  price: 50,
+                  items:[{price: 50}]
+                },
+                {
+                  id:4,
+                  name: 'Cheese sauce',
+                  imageUrl: "https://media.istockphoto.com/id/1413684626/ru/%D1%84%D0%BE%D1%82%D0%BE/%D0%BA%D0%BB%D0%B0%D1%81%D1%81%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B0%D1%8F-%D0%BF%D0%B8%D1%86%D1%86%D0%B0-%D0%BF%D0%B5%D0%BF%D0%BF%D0%B5%D1%80%D0%BE%D0%BD%D0%B8-%D1%81-%D0%BD%D0%B0%D1%80%D0%B5%D0%B7%D0%B0%D0%BD%D0%BD%D1%8B%D0%BC%D0%B8-%D0%BB%D0%BE%D0%BC%D1%82%D0%B8%D0%BA%D0%B0%D0%BC%D0%B8-%D0%B2%D1%8B%D0%B4%D0%B5%D0%BB%D0%B5%D0%BD%D0%BD%D1%8B%D0%BC%D0%B8-%D0%BD%D0%B0-%D0%B1%D0%B5%D0%BB%D0%BE%D0%BC.jpg?s=612x612&w=0&k=20&c=8s9XzBQNFdHBZPxzukGFXNk5jICoBrEb5wA0TZ3vfgk=", 
+                  price: 50,
+                  items:[{price: 50}]
+                },
+                {
+                  id:5,
+                  name: 'Cheese sauce',
+                  imageUrl: "https://media.istockphoto.com/id/1413684626/ru/%D1%84%D0%BE%D1%82%D0%BE/%D0%BA%D0%BB%D0%B0%D1%81%D1%81%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B0%D1%8F-%D0%BF%D0%B8%D1%86%D1%86%D0%B0-%D0%BF%D0%B5%D0%BF%D0%BF%D0%B5%D1%80%D0%BE%D0%BD%D0%B8-%D1%81-%D0%BD%D0%B0%D1%80%D0%B5%D0%B7%D0%B0%D0%BD%D0%BD%D1%8B%D0%BC%D0%B8-%D0%BB%D0%BE%D0%BC%D1%82%D0%B8%D0%BA%D0%B0%D0%BC%D0%B8-%D0%B2%D1%8B%D0%B4%D0%B5%D0%BB%D0%B5%D0%BD%D0%BD%D1%8B%D0%BC%D0%B8-%D0%BD%D0%B0-%D0%B1%D0%B5%D0%BB%D0%BE%D0%BC.jpg?s=612x612&w=0&k=20&c=8s9XzBQNFdHBZPxzukGFXNk5jICoBrEb5wA0TZ3vfgk=", 
+                  price: 50,
+                  items:[{price: 50}]
+                }
+              ]} 
+              categoryId={2}/>
             </div>
           </div>
         </div>
--- components/shared/products-group-list.tsx
+++ components/shared/products-group-list.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+'use client'
+import React, { useEffect } from "react";
+import { useIntersection } from 'react-use';
 import { Title } from "./title";
 import { cn } from "@/lib/utils";
 import { ProductCard } from "./product-card";
+import { useCategoryStore } from "@/store/category";
+
 
 interface Props {
     title: string;
@@ -12,7 +16,22 @@ interface Props {
   }
 
 export const ProductsGroupList: React.FC<Props> = ({ className, title, items, categoryId, listClassName }) => {
-  return <div className={className}>
+
+  const setActiveCategoryId = useCategoryStore((state) => state.setActiveId)
+  const intersectionRef = React.useRef(null);
+  const intersection = useIntersection(intersectionRef, {
+    threshold: 0.4,
+  });
+
+  useEffect(()=>{
+    if (intersection?.isIntersecting) {
+      setActiveCategoryId(categoryId)
+    }
+
+  }, [categoryId, intersection?.isIntersecting, title])
+
+
+  return <div className={className} id={title} ref={intersectionRef}>
 
     <Title text={title} size="lg" className="font-extrabold mb-5" />
     <div className={cn("grid grid-cols-3 gap-[50px]", listClassName)}>
